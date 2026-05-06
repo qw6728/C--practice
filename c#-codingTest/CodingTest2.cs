9f11d342ad1c14b8644133e54c096aa0c50d2e6a
찬혁 윤 <jomecena26@naver.com>
2026-05-06 23:53:28 +0900

    feat: 코딩테스트 문제

---
+++ c#-codingTest/CodingTest2.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+class Student
+{
+    public String Name;
+    public int Score;
+
+}
+
+public class CodingTest2
+{
+    public static void Main()
+    {
+        Student[] student = new Student[3];
+        {
+            student[0] = new Student();
+            student[0].Name = "영의";
+            student[0].Score = 90;
+
+            student[1] = new Student();
+            student[1].Name = "철수";
+            student[1].Score = 80;
+
+            student[2] = new Student();
+            student[2].Name = "바버";
+            student[2].Score = 44;
+        }
+
+        int max = student[0].Score;
+        string topName = student[0].Name;
+
+        for (int i = 0; i < student.Length; i++)
+        {
+            if (student[i].Score > max)
+            {
+                max = student[i].Score;
+                topName = student[i].Name;
+            }
+        }
+
+        Console.WriteLine("1등" + topName);
+
+    }
+}
